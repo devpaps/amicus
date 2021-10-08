5a0a94cb15f745353f53d88ba6ccfd8136cb431e
Lars Jönsson <crymeat@gmail.com>
2021-10-08 12:58:02 +0200

    Removed old files

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
 // Middleware, to use req.body.
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/dist`));
 
 /* app.use((req, res, next) => {
 	console.log(req.headers);
